90d9bddca783f55e5fb8d9d3a7fef91215923bf8
meres <murakozy.nora@gmail.com>
2019-05-10 00:01:19 -0700

    2 test updated with mockito tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,38 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockTorpedoStore1;
+  private TorpedoStore mockTorpedoStore2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockTorpedoStore1 = mock(TorpedoStore.class);
+    mockTorpedoStore2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTorpedoStore1, mockTorpedoStore2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockTorpedoStore1.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockTorpedoStore1).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockTorpedoStore1.fire(1)).thenReturn(true);
+    when(mockTorpedoStore2.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockTorpedoStore1).fire(1);
+    verify(mockTorpedoStore2).fire(1);
   }
 
 }
